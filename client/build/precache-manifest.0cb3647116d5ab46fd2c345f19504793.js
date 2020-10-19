6b1660cab7927eb497507998f77a347789df25d3
kimlimjustin <kimlimjustin@gmail.com>
2020-10-19 10:46:13 +0700

    run `npm run build` on client side

---
+++ client/build/precache-manifest.0cb3647116d5ab46fd2c345f19504793.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b15552bfa30c11963dc81fda724f4ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b32f65b03806e6f06e",
+    "url": "/static/css/2.84cad720.chunk.css"
+  },
+  {
+    "revision": "c6b32f65b03806e6f06e",
+    "url": "/static/js/2.7beff2c8.chunk.js"
+  },
+  {
+    "revision": "e19ae4cbeb1579b0b1f02427faf056dc",
+    "url": "/static/js/2.7beff2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc7c71f8820e503edd7",
+    "url": "/static/js/main.a8320616.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "fbb70cdaa803b59575970adcbce74984",
+    "url": "/static/media/Logout.fbb70cda.jpg"
+  },
+  {
+    "revision": "50cafb34f10ff35a4c09be1c32de088d",
+    "url": "/static/media/chat.50cafb34.png"
+  },
+  {
+    "revision": "84ae5f4800207c5b2d2a9e2344f54c1f",
+    "url": "/static/media/comment.84ae5f48.png"
+  },
+  {
+    "revision": "4bf044ca84d8348c11a515e6416b4a24",
+    "url": "/static/media/profile.4bf044ca.png"
+  }
+]);
